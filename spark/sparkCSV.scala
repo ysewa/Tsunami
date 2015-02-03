892f4971a0dc1902d7c5f1308fbe235dc6e7c81b
ysewa <ysewanono@gmail.com>
2015-02-03 17:56:09 +0100

    new spark sparse

--- spark/sparkCSV.scala
+++ spark/sparkCSV.scala
@@ -27,21 +27,25 @@ def dropHeader(data: RDD[String]): RDD[String] = {
 
 val withoutHeader: RDD[String] = dropHeader(JapanData)
 
-val result = withoutHeader.mapPartitions(lines => {
-         val parser = new CSVParser(',')
+val result = JapanData.mapPartitions(lines => {
+         val parser = new CSVParser(';')
          lines.map(line => {
            val columns = parser.parseLine(line)
-           Array(ArrondisDate(columns(0),simpleDateFormat),columns(2).substring(0,3),columns(5),columns(3),columns(4)).mkString(",")
+           (ArrondisDate(columns(0),simpleDateFormat),columns(1).substring(0,3),columns(4),columns(2),columns(3))
          })
        })
+       
 
 
 result.collect();
 
 /*
-val input = sc.textFile("data_1MB.csv")
-val result = input.map{ line => val reader = new CSVReader(new StringReader(line));reader.readNext();}
+import java.io.StringReader
+import au.code.bytecode.opencsv.CSVReader
 
-val transform = result.map{x=> (ArrondisDate(x(0),simpleDateFormat),x(2).substring(0,3),x(5),x(4),x(5))}
+
+val result = JapanData.map{ line => val reader = new CSVReader(new StringReader(line));reader.readNext();}
+
+val transform = result.map{x=> (ArrondisDate(x(0),simpleDateFormat),x(1).substring(0,3),x(4),x(2),x(3))}
 
 */

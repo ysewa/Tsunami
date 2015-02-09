ed7b9d605cc88180aa942b8922b5abe91398402b
florianriche <friche10@gmail.com>
2015-02-09 02:00:20 +0100

    Update sparkCSV.scala

--- spark/sparkCSV.scala
+++ spark/sparkCSV.scala
@@ -19,25 +19,56 @@ tmp.setMinutes(minutes+10-minutes%10);
 tmp.setSeconds(0);
 return y.format(tmp);
 }
+/**
+ * CREATE KEYSPACE test WITH replication = {
+  'class': 'NetworkTopologyStrategy',
+  'Analytics': '5'
+};
+**/
 
 
 //val JapanData = sc.textFile("data_1MB.csv",5).cache()
 
-
+//Test avec nouvelle structure de table : ((t,id_ville),lat,longi,set(Tel))
 val result = JapanData.mapPartitions(lines => {
          val parser = new CSVParser(';')
          lines.map(line => {
            val columns = parser.parseLine(line)
            ((columns(0).substring(0,15)+"0:00",columns(1).substring(0,3),columns(2).toFloat,columns(3).toFloat),(columns(4).toInt))
          })
-       }).groupByKey().map(x=>(x._1._1,x._1._2,x._1._3,x._1._4,x._2)).saveToCassandra("test","test_spark_set")
+       }).groupByKey(10).map(x=>(x._1._1,x._1._2,x._1._3,x._1._4,x._2)).saveToCassandra("test","test_spark_set")
   
-  //Commandes tests     
-result.groupByKey(20).map(x=>(x._1._1,x._1._2,x._1._3,x._1._4,x._2)).take(2)
-val resultWithKey = result.map(x => (x._1+" "+x._2,x))
-val sortedresultWithKey = resultWithKey.sortByKey()
+  //Commandes tests 
+//Test avec nouvelle structure de table : ((t,id_ville),lat,longi,concatenation text : 3 minutes
 
+val result = JapanData.mapPartitions(lines => {
+         val parser = new CSVParser(';')
+         lines.map(line => {
+           val columns = parser.parseLine(line)
+           ((columns(0).substring(0,15)+"0:00",columns(1).substring(0,3),columns(2),columns(3)),(columns(4)))
+         })
+       }).reduceByKey(_+_,10).map(x=>(x._1._1,x._1._2,x._1._3,x._1._4,x._2)).saveToCassandra("test","test_spark_texttels")
+  
 
+//Test avec nouvelle structure de table : ((t,id_ville),set(text : (lat+"/"longi+"/"+(Tel) 5,7 minutes
+val result = JapanData.mapPartitions(lines => {
+         val parser = new CSVParser(';')
+         lines.map(line => {
+           val columns = parser.parseLine(line)
+           ((columns(0).substring(0,15)+"0:00",columns(1).substring(0,3)),(columns(2)+"/"+columns(3)+"/"+columns(4)))
+         })
+       }).groupByKey(10).map(x=>(x._1._1,x._1._2,x._2)).saveToCassandra("test","test_spark_unique_set")
+  
+
+//Test avec nouvelle structure de table : ((t,id_ville),Text ( grosse concaténation=) : 22secondes
+val result = JapanData.mapPartitions(lines => {
+         val parser = new CSVParser(';')
+         lines.map(line => {
+           val columns = parser.parseLine(line)
+           ((columns(0).substring(0,15)+"0:00",columns(1).substring(0,3)),(columns(2)+"/"+columns(3)+"/"+columns(4)+"|"))
+         })
+       }).reduceByKey(_ + _,10).map(x=>(x._1._1,x._1._2,x._2)).saveToCassandra("test","test_spark_bigText")
+  
 
 result.saveToCassandra("kspace","tableName")
 

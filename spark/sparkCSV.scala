002a248fa082094d0c0393f38e5d36aa4655f26d
florianriche <friche10@gmail.com>
2015-02-11 08:58:42 +0100

    Update sparkCSV.scala

--- spark/sparkCSV.scala
+++ spark/sparkCSV.scala
@@ -39,7 +39,7 @@ sc.getConf.set("spark.cassandra.output.concurrent.writes","10")
 //create table test_spark_set(t timestamp, id_ville text, lat float, longi float,tels set<int>, primary key ((t,id_ville),lat));
 //1Go : 32 secondes de mapPartition  + 10 minutes
 //pas réussi à intégrer avec les R2
-val result = JapanData.mapPartitions(lines => {
+/*val result = JapanData.mapPartitions(lines => {
          val parser = new CSVParser(';')
          lines.map(line => {
            val columns = parser.parseLine(line)
@@ -47,31 +47,30 @@ val result = JapanData.mapPartitions(lines => {
          })
        }).groupByKey(30).map(x=>(x._1._1,x._1._2,x._1._3,x._1._4,x._2)).saveToCassandra("test","test_spark_set")
   
-  
+  */
 //Commandes tests 
 //create table test_spark_textTels(t timestamp, id_ville text,lat float,longi float,tel text, primary key((t,id_ville)));
 
 //Test avec nouvelle structure de table : ((t,id_ville),lat,longi,concatenation text : 3 minutes +++
-val result = JapanData.mapPartitions(lines => {
+/*val result = JapanData.mapPartitions(lines => {
          val parser = new CSVParser(';')
          lines.map(line => {
            val columns = parser.parseLine(line)
            ((columns(0).substring(0,15)+"0:00",columns(1).substring(0,3),columns(2),columns(3)),(columns(4)+"|"))
-         })   }).reduceByKey(_+_,24).map(x=>(x._1._1,x._1._2,x._1._3,x._1._4,x._2)).saveToCassandra("test","test_spark_texttels")
-    
-
+         })   }).reduceByKey(_+_,24).map(x=>(x._1._1,x._1._2,x._1._3,x._1._4,x._2)).saveToCassandra("test","test_spark_texttels")*/
+         
 //Test avec nouvelle structure de table : ((t,id_ville),set(text : (lat+"/"longi+"/"+(Tel) 
 //create table test_spark_unique_set(t timestamp, id_ville text, tels set<text>, primary key ((t,id_ville)));
 //
 //map : 1 minutes 
 //cassandra : 3 minutes
-val result = JapanData.mapPartitions(lines => {
+/*val result = JapanData.mapPartitions(lines => {
          val parser = new CSVParser(';')
          lines.map(line => {
            val columns = parser.parseLine(line)
            ((columns(0).substring(0,15)+"0:00",columns(1).substring(0,3)),(columns(2)+"/"+columns(3)+"/"+columns(4)))
          })
-       }).groupByKey(24).map(x=>(x._1._1,x._1._2,x._2)).saveToCassandra("test","test_spark_unique_set")
+       }).groupByKey(24).map(x=>(x._1._1,x._1._2,x._2)).saveToCassandra("test","test_spark_unique_set")*/
   
 
 //Test avec nouvelle structure de table : ((t,id_ville),Text ( grosse concaténation=) :  
